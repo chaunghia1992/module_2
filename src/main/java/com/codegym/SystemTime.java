d245f26dc93f0b7585d38a78adb506fb08a6e620
chaunghia1992 <nchau0992@gmail.com>
2023-03-08 16:43:53 +0700

    kbbien.java

--- src/main/java/com/codegym/SystemTime.java
+++ src/main/java/com/codegym/SystemTime.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class SystemTime {
     public static void main(String[] args) {
-        Date now = new Date(); // Lấy ra thời gian hiện tại
+        Date now = new Date();
 
         System.out.println("Now is: " + now);
     }
