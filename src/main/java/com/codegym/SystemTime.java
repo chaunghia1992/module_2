2b5ebd009eff6a627cf32168e31631def0c8615e
chaunghia1992 <nchau0992@gmail.com>
2023-03-08 16:26:29 +0700

    sytemtime.java

---
+++ src/main/java/com/codegym/SystemTime.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
