4fe884301952bedbe08b61c1f8720ac37850a2e2
chaunghia1992 <nchau0992@gmail.com>
2023-03-10 13:36:19 +0700

    showtypes2

--- src/main/java/com/codegym/Showtypes/Showtypes1.java
+++ src/main/java/com/codegym/Showtypes/Showtypes1.java
@@ -2,16 +2,16 @@ package com.codegym.Showtypes;
 
 public class Showtypes1 {
     public static void main(String[] args) {
-        main();
-    }
-    public static int size = 7;
-    public static void main() {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < size; j++) {
-                System.out.print("*  ");
+        main();}
+
+        public static int size = 7;
+        public static void main () {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < size; j++) {
+                    System.out.print("*  ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
-}
 
--- src/main/java/com/codegym/Showtypes/Showtypes2.java
+++ src/main/java/com/codegym/Showtypes/Showtypes2.java
@@ -1,4 +1,15 @@
 package com.codegym.Showtypes;
 
 public class Showtypes2 {
+public static void main(String[] args){
+    for (int i = 0;i < 5;i++){
+        for (int j = 0;j <= i;j++){
+            System.out.print("*  ");
+        }
+        System.out.println();
+    }
+
+
 }
+}
+
