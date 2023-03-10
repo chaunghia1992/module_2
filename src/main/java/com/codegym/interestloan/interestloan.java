50b685d19b1020b8a5028dda6f631198dcdb4fbf
chaunghia1992 <nchau0992@gmail.com>
2023-03-10 17:17:20 +0700

    interestloan.java

---
+++ src/main/java/com/codegym/interestloan/interestloan.java
@@ -0,0 +1,16 @@
+package com.codegym.interestloan;
+import java.util.Scanner;
+public class interestloan {
+    public static void main(String[] args){
+        double monney = 1.0;
+        int month = 1 ;
+        double interestloan =1.0;
+        Scanner input = new Scanner(System.in);
+        System.out.println(" Enter investment amount:");
+        monney = input.nextDouble();
+        System.out.println(" Enter number of months:");
+        month = input.nextInt();
+        System.out.println("Enter annual interest rate in percentage:");
+
+    }
+}
