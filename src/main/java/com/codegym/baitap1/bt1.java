8afd8ada06161865422fab6afcdb41f8b8defef3
chaunghia1992 <nchau0992@gmail.com>
2023-03-13 13:40:16 +0700

    bt1

---
+++ src/main/java/com/codegym/baitap1/bt1.java
@@ -0,0 +1,43 @@
+package com.codegym.baitap1;
+
+import javax.swing.*;
+
+//bt1
+public class bt1 {
+
+    static int numbers[] = {1,7,5,1};
+
+    public static void main(String[] args) {
+//
+//        int sum = 0;
+//        for (int num : numbers) {
+//            sum = num + sum;
+//
+//        }
+//        System.out.println("Tổng kết quả của mảng " + sum);
+
+        //Bt2
+
+//        for (int i = 0; i < numbers.length; i++) {
+//            if(numbers[i] % 2 == 0){
+//                System.out.println(numbers[i] + " là số chẵn");
+//            }
+//        }
+
+
+        //bai3:
+//        int min = numbers[0];
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (min > numbers[i]) {
+//                min = numbers[i];
+//            }
+//        }
+//        System.out.println(min);
+
+        //BT4:
+        //numbers[4] = 6;
+        //for (int i = 0; i < numbers.length; i++) {
+           // System.out.println(numbers);
+        }
+    }
+}
