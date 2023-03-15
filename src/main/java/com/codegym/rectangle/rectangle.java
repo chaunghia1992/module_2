ffb9bc2ebe8f75d2171986ab70d4549bb6760764
chaunghia1992 <nchau0992@gmail.com>
2023-03-15 14:25:02 +0700

    rectangle.java

---
+++ src/main/java/com/codegym/rectangle/rectangle.java
@@ -0,0 +1,36 @@
+package com.codegym.rectangle;
+
+import java.util.Scanner;
+
+public class rectangle {
+    double width, height;
+    public rectangle(){
+    }
+    public rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+    public double getAreA(){
+        return this.width * this.height;
+    }
+    public double getPerimeter(){
+        return (this.width + this.height) * 2;
+    }
+    public String display(){
+        return "rectangle{" + "width=" + width + ",height=" + height + "}";
+
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the width:");
+        double width = scanner.nextDouble();
+        System.out.println("Enter the height");
+        double height = scanner.nextDouble();
+        rectangle rectangle = new rectangle(width, height);
+        System.out.println("Your Rectangle \n"+ rectangle.display());
+        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ rectangle.getAreA());
+
+    }
+}
