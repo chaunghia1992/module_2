9829361e6dc303f61c7324bddae5db05b9a07c4c
chaunghia1992 <nchau0992@gmail.com>
2023-03-17 08:42:10 +0700

    findwishes1.java

---
+++ src/main/java/com/codegym/findwishes/findwishes1.java
@@ -0,0 +1,16 @@
+package com.codegym.findwishes;
+
+import java.util.Scanner;
+
+public class findwishes1 {
+    public static void main(String[] args){
+        int a ;
+        int b ;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a : ");
+        a = input.nextInt();
+        System.out.println("Enter b : ");
+        b = input.nextInt();
+
+    }
+}
