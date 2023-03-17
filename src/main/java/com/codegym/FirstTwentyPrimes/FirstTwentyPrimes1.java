5146d2649017ee442884ff31ff30b66148748dc4
chaunghia1992 <nchau0992@gmail.com>
2023-03-17 09:29:31 +0700

    firstwentyprimes1.java

---
+++ src/main/java/com/codegym/FirstTwentyPrimes/FirstTwentyPrimes1.java
@@ -0,0 +1,23 @@
+package com.codegym.FirstTwentyPrimes;
+import java.util.Scanner;
+public class FirstTwentyPrimes1 {
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2;
+
+        while (count < 20) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(number + " ");
+                count++;
+            }
+            number++;
+        }
+    }
+}
