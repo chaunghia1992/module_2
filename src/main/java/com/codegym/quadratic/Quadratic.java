d4ff5ea855fd4986f017056ddb8da77676a07768
chaunghia1992 <nchau0992@gmail.com>
2023-03-21 16:32:26 +0700

    quadratic.java

---
+++ src/main/java/com/codegym/quadratic/Quadratic.java
@@ -0,0 +1,52 @@
+package com.codegym.quadratic;
+
+public class Quadratic {
+    private double a, b, c;
+
+    public Quadratic(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getDiscriminant() {
+        return b * b - 4 * a * c;
+    }
+
+    public double getRoot1() {
+        double discriminant = getDiscriminant();
+        if (discriminant < 0) {
+            return 0;
+        }
+        return (-b + Math.sqrt(discriminant)) / (2 * a);
+    }
+
+    public double getRoot2() {
+        double discriminant = getDiscriminant();
+        if (discriminant < 0) {
+            return 0;
+        }
+        return (-b - Math.sqrt(discriminant)) / (2 * a);
+    }
+
+    public static void main(String[] args) {
+        Quadratic equation = new Quadratic(1, -5, 6);
+        double root1 = equation.getRoot1();
+        double root2 = equation.getRoot2();
+        System.out.println("Root 1: " + root1);
+        System.out.println("Root 2: " + root2);
+    }
+}
+
