664a300db9d64ed4948683dcb6b4c36cf446642d
chaunghia1992 <nchau0992@gmail.com>
2023-03-21 08:45:19 +0700

    SNTmin100.java

---
+++ src/main/java/com/codegym/look100/look2100.java
@@ -0,0 +1,26 @@
+package com.codegym.look100;
+
+import java.util.Scanner;
+public class look2100 {
+    public static void main(String[] args){
+        int point = 0;
+        int count = 0;
+        System.out.println("các số nguyên tố từ 2 đến 100:");
+        for (int  N = 2 ; N <100; N++){
+            if(point < 100){
+                for (int  j = 1 ; j <= N; j ++){
+                    if (N % j == 0){
+                        count += 1;
+                    }
+                }
+                if (count == 2 ){
+                    System.out.println(N);
+                }
+                count = 0;
+            }else {
+                break;
+            }
+        }
+    }
+
+}
