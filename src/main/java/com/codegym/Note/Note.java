45757d877671acb55ac235e49a9c704a6112b82d
chaunghia1992 <nchau0992@gmail.com>
2023-04-27 11:35:34 +0700

    linkedlistqueue.java

---
+++ src/main/java/com/codegym/Note/Note.java
@@ -0,0 +1,10 @@
+package com.codegym.Note;
+
+public class Note {
+    public int key;
+    public Note next;
+    public Note (int key){
+        this.key = key;
+        this.next = null ;
+    }
+}
