e3bcd0e1bfde6a07f5036963a5ca8827af9314d9
chaunghia1992 <nchau0992@gmail.com>
2023-04-27 11:16:27 +0700

    linkedlist.java

---
+++ src/case/model/Dairycow.kt
@@ -0,0 +1,4 @@
+package case.model
+
+class Dairycow {
+}
---
+++ src/main/java/com/codegym/text/view/Dairycowmain.java
@@ -0,0 +1,18 @@
+package com.codegym.text.model;
+
+import java.util.ArrayList;
+
+public class Dairycowmain {
+
+    public static void main(String[] args) {
+        ArrayList <Dairycow> arrayList = new ArrayList<>();
+        Dairycow l1 = new Dairycow(1,"bò sữa loại 1 ",5000,3,40,10);
+        Dairycow l2 = new Dairycow(2,"bò sữa loại 2 ",4000,4,50,9);
+        Dairycow l3 = new Dairycow(3,"bò sữa loại 3 ",3000,5,60,8);
+        Dairycow l4 = new Dairycow(4,"bò sữa loại 4 ",2000,6,70,7);
+        Dairycow l5 = new Dairycow(5,"bò sữa loại 5 ",1000,7,75,6);
+
+
+    }
+
+}
